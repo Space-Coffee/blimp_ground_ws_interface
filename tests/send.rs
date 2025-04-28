21dfb5712e9c9fa7532564a3389909a9fbe12f74
Asapros <asapros@wp.pl>
2025-04-28 18:41:27 +0200

    Simplified test_client_send with oneshot

--- tests/send.rs
+++ tests/send.rs
@@ -1,36 +1,37 @@
 use std::sync::Arc;
-use std::time::Duration;
-use tokio::sync::{Mutex, Notify};
-use tokio::time::timeout;
+use futures_util::SinkExt;
+use tokio::sync::{Mutex, oneshot};
+use tokio::time::{timeout, Duration};
 use blimp_ground_ws_interface::{BlimpGroundWebsocketClient, BlimpGroundWebsocketServer, MessageV2G, VizInterest};
 
 #[tokio::test]
 async fn test_client_send() {
-    let received = Arc::new(Mutex::new(None));
-    let notify = Arc::new(Notify::new());
+    let (tx, rx) = oneshot::channel();
+    let tx = Arc::new(Mutex::new(Some(tx)));
 
-    let moved_received = received.clone();
-    let moved_notify = notify.clone();
+    let server = {
+        let tx = tx.clone();
+        tokio::spawn(async move {
+            let mut server = BlimpGroundWebsocketServer::new("localhost:9999");
+            server.bind().await.expect("Failed address bind");
+            server.run(move |message| {
+                let tx = tx.clone();
+                async move {
+                    tx.lock().await.take().expect("Tx already taken").send(message).expect("Failed to send");
+                }
+            }).await.expect("Server failed");
+        })
+    };
 
-    let running = tokio::spawn(async move {
-        let mut server = BlimpGroundWebsocketServer::new("localhost:9999");
-        server.bind().await.expect("Failed address bind");
-        server.run(move |message| {
-            let value = moved_received.clone();
-            let notify = moved_notify.clone();
-            async move {
-                *value.lock().await = Some(message);
-                notify.notify_one();
-            }
-        }).await.expect("Server failed");
-    });
     let mut client = BlimpGroundWebsocketClient::new("ws://localhost:9999/");
-    let sent = MessageV2G::DeclareInterest(VizInterest{motors: true, servos: true, sensors: true});
+    let sent = MessageV2G::DeclareInterest(VizInterest { motors: true, servos: true, sensors: true });
+
     client.connect().await.expect("Failed to connect the client");
-    client.send(sent).await.expect("Failed to send the client message");
+    client.send(sent.clone()).await.expect("Failed to send the client message");
 
-    timeout(Duration::from_secs(1), notify.notified()).await.expect("Timed out waiting for client message");
+    let received = timeout(Duration::from_secs(1), rx).await.expect("Timed out waiting for client message").expect("Receive failed");
 
-    running.abort();
-    assert_eq!(sent, received.lock().await.unwrap());
+    server.abort();
+
+    assert_eq!(sent, received);
 }

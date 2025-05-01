f82137dc275db11df58e13640a11bc4cfe030de4
Tomasz Kawalec <tkawalec369@gmail.com>
2025-05-01 13:18:37 +0200

    Alias tokio::sync::Mutex as TMutex, to avoid confusion with std::sync::Mutex

--- tests/send.rs
+++ tests/send.rs
@@ -1,15 +1,21 @@
-use blimp_ground_ws_interface::{BlimpGroundWebsocketClient, BlimpGroundWebsocketServer, MessageG2V, MessageV2G, VizInterest};
+use blimp_ground_ws_interface::{
+    BlimpGroundWebsocketClient, BlimpGroundWebsocketServer, MessageG2V, MessageV2G, VizInterest,
+};
 use std::net::SocketAddr;
 use std::sync::Arc;
-use tokio::sync::{oneshot, Mutex};
+use tokio::sync::{oneshot, Mutex as TMutex};
 use tokio::time::{timeout, Duration};
 
-const CLIENT_MESSAGE: MessageV2G = MessageV2G::DeclareInterest(VizInterest { motors: true, servos: true, sensors: true });
+const CLIENT_MESSAGE: MessageV2G = MessageV2G::DeclareInterest(VizInterest {
+    motors: true,
+    servos: true,
+    sensors: true,
+});
 #[tokio::test]
 async fn test_client_send() {
     let (tx, rx) = oneshot::channel();
-    let tx = Arc::new(Mutex::new(Some(tx)));
-    
+    let tx = Arc::new(TMutex::new(Some(tx)));
+
     let (address_tx, address_rx) = oneshot::channel::<SocketAddr>();
 
     let server = {
@@ -17,24 +23,46 @@ async fn test_client_send() {
         tokio::spawn(async move {
             let mut server = BlimpGroundWebsocketServer::new("localhost:0");
             server.bind().await.expect("Failed address bind");
-            address_tx.send(server.get_address().unwrap()).expect("Did not send the address properly");
-            server.run(move |mut pair| {
-                let tx = tx.clone();
-                async move {
-                    let message= pair.recv::<MessageV2G>().await.expect("Failed to receive client message");
-                    tx.lock().await.take().expect("Tx already taken").send(message).expect("Failed to send");
-                }
-            }).await.expect("Server failed");
+            address_tx
+                .send(server.get_address().unwrap())
+                .expect("Did not send the address properly");
+            server
+                .run(move |mut pair| {
+                    let tx = tx.clone();
+                    async move {
+                        let message = pair
+                            .recv::<MessageV2G>()
+                            .await
+                            .expect("Failed to receive client message");
+                        tx.lock()
+                            .await
+                            .take()
+                            .expect("Tx already taken")
+                            .send(message)
+                            .expect("Failed to send");
+                    }
+                })
+                .await
+                .expect("Server failed");
         })
     };
-    
+
     let address = address_rx.await.expect("Failed to receive target address");
     let mut client = BlimpGroundWebsocketClient::new(format!("ws://{}", address).as_str());
 
-    client.connect().await.expect("Failed to connect the client");
-    client.send(CLIENT_MESSAGE).await.expect("Failed to send the client message");
+    client
+        .connect()
+        .await
+        .expect("Failed to connect the client");
+    client
+        .send(CLIENT_MESSAGE)
+        .await
+        .expect("Failed to send the client message");
 
-    let received = timeout(Duration::from_secs(1), rx).await.expect("Timed out waiting for client message").expect("Receive failed");
+    let received = timeout(Duration::from_secs(1), rx)
+        .await
+        .expect("Timed out waiting for client message")
+        .expect("Receive failed");
 
     server.abort();
 
@@ -49,22 +77,33 @@ async fn test_server_send() {
         tokio::spawn(async move {
             let mut server = BlimpGroundWebsocketServer::new("localhost:0");
             server.bind().await.expect("Failed address bind");
-            address_tx.send(server.get_address().unwrap()).expect("Did not send the address properly");
-            server.run(|mut pair| {
-                async move {
-                    pair.send(SERVER_MESSAGE).await.expect("Failed to send server message");
+            address_tx
+                .send(server.get_address().unwrap())
+                .expect("Did not send the address properly");
+            server
+                .run(|mut pair| async move {
+                    pair.send(SERVER_MESSAGE)
+                        .await
+                        .expect("Failed to send server message");
                     pair.close().await.expect("Failed to close");
-                }
-            }).await.expect("Server failed");
+                })
+                .await
+                .expect("Server failed");
         })
     };
-    
+
     let address = address_rx.await.expect("Failed to receive target address");
     let mut client = BlimpGroundWebsocketClient::new(format!("ws://{}", address).as_str());
 
-    client.connect().await.expect("Failed to connect the client");
+    client
+        .connect()
+        .await
+        .expect("Failed to connect the client");
 
-    let received = timeout(Duration::from_secs(1), client.recv()).await.expect("Timed out waiting for client message").expect("Receive failed");
+    let received = timeout(Duration::from_secs(1), client.recv())
+        .await
+        .expect("Timed out waiting for client message")
+        .expect("Receive failed");
 
     server.abort();
 

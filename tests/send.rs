2f6fb8ab1a183632595a55fcc3a8029d6ff07e74
Asapros <cyanowakredka@gmail.com>
2025-05-12 13:39:55 +0200

    Add run_client

--- tests/send.rs
+++ tests/send.rs
@@ -8,7 +8,7 @@ use tokio::time::{timeout, Duration};
 
 use blimp_ground_ws_interface::{BlimpGroundWebsocketClient, BlimpGroundWebsocketServer, BlimpGroundWebsocketStreamPair, BlimpSubprotocol, BlimpSubprotocolFlavour, MessageG2V, MessageV2G, VizInterest};
 
-async fn start_server<F, Fut>(address_tx: oneshot::Sender<SocketAddr>, handler: F)
+async fn run_server<F, Fut>(address_tx: oneshot::Sender<SocketAddr>, handler: F)
 where
     F: Fn(BlimpGroundWebsocketStreamPair<TcpStream>) -> Fut + Send + Sync + 'static,
     Fut: Future<Output = ()> + Send + 'static,
@@ -19,6 +19,19 @@ where
         .send(server.get_address().unwrap())
         .expect("Did not send the address properly");
     server.run(handler).await.expect("Server failed");
+}
+async fn run_client<F, Fut>(address_rx: oneshot::Receiver<SocketAddr>, handler: F)
+where
+    F: Fn(&BlimpGroundWebsocketClient) -> Fut + Send + Sync,
+    Fut: Future<Output = ()> + Send,
+{
+    let address = address_rx.await.expect("Failed to receive target address");
+    let mut client = BlimpGroundWebsocketClient::new(format!("ws://{}", address).as_str());
+    client.connect().await.expect("Failed to connect the client");
+
+    handler(&client).await;
+
+    client.disconnect().await.expect("Failed to disconnect the client");
 }
 
 const CLIENT_MESSAGE: MessageV2G = MessageV2G::DeclareInterest(VizInterest {
@@ -35,7 +48,7 @@ async fn test_client_send() {
 
     let server = {
         let tx = tx.clone();
-        tokio::spawn(start_server(address_tx,move |pair| {
+        tokio::spawn(run_server(address_tx, move |pair| {
             let tx = tx.clone();
             async move {
                 let message = pair
@@ -69,9 +82,9 @@ async fn test_client_send() {
         .expect("Timed out waiting for client message")
         .expect("Receive failed");
 
-    server.abort();
-
     assert_eq!(CLIENT_MESSAGE, received);
+
+    server.abort();
 }
 
 #[tokio::test]
@@ -83,7 +96,7 @@ async fn test_client_send_json() {
 
     let server = {
         let tx = tx.clone();
-        tokio::spawn(start_server(address_tx, move |pair| {
+        tokio::spawn(run_server(address_tx, move |pair| {
             let tx = tx.clone();
             async move {
                 let message = pair
@@ -128,7 +141,7 @@ const SERVER_MESSAGE: MessageG2V = MessageG2V::MotorSpeed { id: 0, speed: 0 };
 async fn test_server_send() {
     let (address_tx, address_rx) = oneshot::channel::<SocketAddr>();
     let server = {
-        tokio::spawn(start_server(address_tx, move |pair| async move {
+        tokio::spawn(run_server(address_tx, move |pair| async move {
                 pair.send(SERVER_MESSAGE).await.expect("Failed to send server message");
                 pair.close().await.expect("Failed to close");
             }
@@ -156,7 +169,7 @@ async fn test_server_send() {
 async fn test_server_send_json() {
     let (address_tx, address_rx) = oneshot::channel::<SocketAddr>();
     let server = {
-        tokio::spawn(start_server(address_tx, move |pair| async move {
+        tokio::spawn(run_server(address_tx, move |pair| async move {
             pair.send(SERVER_MESSAGE).await.expect("Failed to send server message");
             pair.close().await.expect("Failed to close");
         }
@@ -184,30 +197,6 @@ async fn test_server_send_json() {
 
 #[tokio::test]
 async fn test_concurrent_sends() {
-    // .run(move |pair| {
-    //     let server_recv_notify = server_recv_notify.clone();
-    //     async move {
-    //         let pair = Arc::new(pair);
-    //         let recv_task = {
-    //             let pair = pair.clone();
-    //             tokio::spawn(async move {
-    //                 for _i in 0..MSGS_COUNT {
-    //                     let msg = pair.recv::<MessageV2G>().await.unwrap();
-    //                     assert_eq!(msg, CLIENT_MESSAGE);
-    //                 }
-    //                 server_recv_notify.notify_one();
-    //             })
-    //         };
-    // 
-    //         for _i in 0..MSGS_COUNT {
-    //             pair.send(SERVER_MESSAGE)
-    //                 .await
-    //                 .expect("Failed to send server message");
-    //         }
-    // 
-    //         recv_task.await.unwrap();
-    //     }
-    // })
     const MSGS_COUNT: usize = 64;
 
     let (address_tx, address_rx) = oneshot::channel::<SocketAddr>();
@@ -216,7 +205,7 @@ async fn test_concurrent_sends() {
 
     let server = {
         let server_recv_notify = server_recv_notify.clone();
-        tokio::spawn(start_server(address_tx, move |pair| {
+        tokio::spawn(run_server(address_tx, move |pair| {
             let server_recv_notify = server_recv_notify.clone();
             async move {
                 let pair = Arc::new(pair);
@@ -230,13 +219,13 @@ async fn test_concurrent_sends() {
                         server_recv_notify.notify_one();
                     })
                 };
-        
+
                 for _i in 0..MSGS_COUNT {
                     pair.send(SERVER_MESSAGE)
                         .await
                         .expect("Failed to send server message");
                 }
-        
+
                 recv_task.await.unwrap();
             }
         }))
